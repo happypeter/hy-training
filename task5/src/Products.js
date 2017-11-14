d03c5465507aef85684259eaff41185587fe076d
Peter Wang <happypeter1983@gmail.com>
2017-11-15 07:34:18 +0800

    coding style #8

--- task5/src/Products.js
+++ task5/src/Products.js
@@ -3,33 +3,37 @@ import React, { Component } from 'react'
 class Products extends Component {
 
   constructor(props) {
-      super(props);
-this.clickHandle = this.clickHandle.bind(this)
-this.state = {
-  cnt:0
-}
+    super(props)
+    this.clickHandle = this.clickHandle.bind(this)
+    this.state = {
+      cnt:0
+    }
   }
 
+  clickHandle(product) {
+    this.props.onUpdateTest(product.name,product.price)
+  }
 
-clickHandle(product) {
-  this.props.onUpdateTest(product.name,product.price)
-}
-countNum(cnt) {
-  cnt++
- this.setState({
-   cnt
- })
-  this.props.onCount(cnt)
-}
-
+  countNum(cnt) {
+    cnt++
+     this.setState({
+       cnt
+     })
+    this.props.onCount(cnt)
+  }
 
   render () {
-
-const list = this.props.products.map((product,i) =><div data-value={product.name} key={i}> <p>{product.name}</p> <p>{product.price}</p><button onClick={this.clickHandle.bind(this,product)}>buy</button> </div>);
+    const list = this.props.products.map((product,i) =>(
+      <div data-value={product.name} key={i}>
+        <p>{product.name}</p>
+        <p>{product.price}</p>
+        <button onClick={this.clickHandle.bind(this,product)}>buy</button>
+      </div>
+    ))
+    
     return (
       <div className='Products'>
         {list}
-
         <button onClick={this.countNum.bind(this,this.state.cnt)}>+</button>
       </div>
     )
