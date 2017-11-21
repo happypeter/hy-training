1a6b067846ca861bc9d491b93b0399c23584349c
Peter Wang <happypeter1983@gmail.com>
2017-11-21 12:20:12 +0800

    reducer function #7

--- task6/reduxshopping/src/redux/reducers/reducer_products.js
+++ task6/reduxshopping/src/redux/reducers/reducer_products.js
@@ -1,7 +1,11 @@
-export default function () {
-    return [
+const initState = [
         {title: 'test1',price:20},
         {title: 'test2',price:30},
         {title: 'test3',price:40},
     ]
+
+const products = (state = initState, action) => {
+  return state
 }
+
+export default products
